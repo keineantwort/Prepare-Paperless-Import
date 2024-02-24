0ad043677565a80d88beadb50c7137b3c3dfe0e9
Martin Schumacher <msc@keineantwort.de>
2024-02-24 11:44:27 +0100

    added pullup

--- log.py
+++ log.py
@@ -1,10 +1,14 @@
 import logging
 
 
-def init_logger(log_level:int = logging.INFO, log_format: str = '%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s'):
+def init_logger(log_level:int = logging.INFO, log_format: str = '%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', logfile: str = None):
     logging.basicConfig(format=log_format,
                         datefmt='%Y-%m-%d:%H:%M:%S',
                         level=log_level)
     log = logging.getLogger(__name__)
+    if logfile:
+        filehandler = logging.FileHandler(logfile)
+        filehandler.setFormatter(logging.Formatter(log_format))
+        log.addHandler(filehandler)
     # LogLevel
     return log
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import logging
 from log import init_logger
 from model import *
 
-log = init_logger(log_level=logging.DEBUG)
+FILEINPUT = "test.yaml"
+log = init_logger(log_level=logging.DEBUG, logfile=f"{FILEINPUT}.log")
 
 
 def read_config(file: str):
@@ -25,23 +26,20 @@ def copy_file(path: str, target: str):
         if os.path.exists(target):
             target_path, target_file = os.path.split(os.path.abspath(target))
             target = os.path.join(target_path, f"{os.path.basename(os.path.dirname(os.path.abspath(path)))} - {target_file}")
-            log.debug("!!!!!!!!! --------- !!!!!!!!")
-        log.debug(f"> copying file {path} to {target}")
+        log.debug(f"  > copying file {path} to {target}")
         shutil.copy2(path, target)
 
 
 def import_documents(import_path: str, target_dir: str = None):
-    log.debug(f"crawling Directory: {import_path}")
-    directory = os.path.basename(os.path.normpath(import_path))
-    if directory in import_config.excludes:
-        log.debug(f"! EXCLUDE {import_path}")
+    log.debug(f" ----- Directory: {import_path} -----")
+    base_path, directory_name = os.path.split(os.path.normpath(import_path))
+    if directory_name in import_config.excludes:
+        log.debug(f"  ! EXCLUDE {import_path}")
         return
-    if directory in import_config.flatten:
+    if directory_name in import_config.flatten:
         target_dir = os.path.join(config.target, os.path.basename(import_path))
-        log.debug(f"Flatten: {target_dir}")
-    if directory in import_config.pullup:
-        log.debug(f"(TODO) Pullup: {import_path}")
-        return
+    if directory_name in import_config.pullup:
+        target_dir = os.path.join(config.target, os.path.basename(base_path))
     for path in [f.path for f in os.scandir(import_path)]:
         if os.path.isdir(path):
             import_documents(path, target_dir)
@@ -49,7 +47,6 @@ def import_documents(import_path: str, target_dir: str = None):
             target_file = os.path.join(config.target, path[len(import_dir) + 1:])
             if target_dir:
                 target_file = os.path.join(target_dir, os.path.basename(os.path.normpath(path)))
-            log.debug(f"{path} > {target_file}")
             copy_file(path, target_file)
 
 
@@ -68,7 +65,7 @@ def prepare_target():
 
 
 if __name__ == '__main__':
-    config = read_config("test.yaml")
+    config = read_config(FILEINPUT)
     prepare_target()
     for import_config in config.imports:
         import_dir = os.path.join(config.root, import_config.dir)
